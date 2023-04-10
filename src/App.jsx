910d8cd25c2a3a3b52587605c94518b95f9fe7b9
Milangela Grimaldo <titagrimaldoo@gmail.com>
2023-04-10 01:20:47 -0400

    llamndo a la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import './App.css'
+import React,{ useState, useEffect } from 'react';
 
-import React from 'react'
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const APIUrl = "https://pokeapi.co/api/v2/pokemon"
+  const APINombres= "https://pokeapi.co/api/v2/pokemon/"
+  const pokemonSeleccionado = "pikachu"
+  const [pokemonenuso, setPokemon] = useState(0);
+  
+  
+  useEffect(() => {
+    fetch(APIUrl)
+      .then(response => response.json())
+      .then(json => {setData(json.results)})
+      .catch(error => console.error(error));
+
+      fetch(APINombres+pokemonSeleccionado)
+      .then(response => response.json())
+      .then(json => {setPokemon(json.weight)})
+      .catch(error => console.error(error));
+
+  }, []);
+
+
   return (
-    <div>Hola</div>
+    <div>
+      <select>
+      {data.map((pokemon, index) => (
+        <option value={index}> {pokemon.name} </option>
+     ))}
+     </select>
+
+       <h1>{pokemonenuso}</h1> 
+      
+  </div>
   )
 }
 
